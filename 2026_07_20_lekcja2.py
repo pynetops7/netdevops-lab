68a8e96649114201953635e2ebd489be81f9c0f5
pynetops7 <pynetops7@users.noreply.github.com>
2026-07-20 20:04:37 +0200

    Aktualizacja lekcja2

--- 2026_07_20_lekcja2.py
+++ 2026_07_20_lekcja2.py
@@ -1,11 +1,11 @@
-host = "grzegorz"
-port = 42
 commands = ["show version", "show vlan", "show interfaces"]
-device = {
-    "model": "Catalyst 9000",
-    "ports": 22
-}
-print(host)
-print(port)
-print(commands)
-print(device)
+
+for cmd in commands:
+    print(cmd)
+
+status = "up"
+
+if status == "up":
+    print("Interfejs działa")
+else:
+    print("Interfejs nie działa")   
