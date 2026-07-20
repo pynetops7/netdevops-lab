c92d100194a2c27fac6e25698f04757ea0bc687e
pynetops7 <pynetops7@users.noreply.github.com>
2026-07-20 17:17:33 +0200

    Reorganizacja plików lekcji z datami

---
+++ 2026_07_20_lekcja1.py
@@ -0,0 +1,11 @@
+host = "grzegorz"
+port = 42
+commands = ["show version", "show vlan", "show interfaces"]
+device = {
+    "model": "Catalyst 9000",
+    "ports": 22
+}
+print(host)
+print(port)
+print(commands)
+print(device)
